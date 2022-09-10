4de4c5af0b078dd60782ea0ec20994a0ce40cc72
JoaoA <joaootsugua@gmail.com>
2022-09-10 18:59:54 -0300

    Implementando ListaIconeComponent

--- ListaComprasAngular/src/app/components/lista-icone/lista-icone.component.ts
+++ ListaComprasAngular/src/app/components/lista-icone/lista-icone.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Descricao } from '../models/descricao.class';
 
 @Component({
@@ -11,13 +13,17 @@ export class ListaIconeComponent implements OnInit{
   
 
   @Input() descricao!: Descricao
-  constructor() { }
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) { 
+
+    // this.matIconRegistry.addSvgIcon('iconeDeletar', 
+    //   this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/icons8-trash-150.svg'))
+  }
 
   ngOnInit(): void {
 
   }
 
-  teste(){
+  deletarLista(){
     console.log("TESTE ", this.descricao)
   }
 
