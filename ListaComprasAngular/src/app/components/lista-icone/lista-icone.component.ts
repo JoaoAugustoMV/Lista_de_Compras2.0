da1438bb9c165befe8dc99ba6063f7e8df8f360c
JoaoA <joaootsugua@gmail.com>
2022-09-10 12:08:02 -0300

    Iniciando a construção do Componente listaIcone

--- ListaComprasAngular/src/app/components/lista-icone/lista-icone.component.ts
+++ ListaComprasAngular/src/app/components/lista-icone/lista-icone.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Descricao } from '../models/descricao.class';
 
 @Component({
   selector: 'app-lista-icone',
   templateUrl: './lista-icone.component.html',
   styleUrls: ['./lista-icone.component.css']
 })
-export class ListaIconeComponent implements OnInit {
+export class ListaIconeComponent implements OnInit{
 
+  
+
+  @Input() descricao!: Descricao
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  teste(){
+    console.log("TESTE ", this.descricao)
   }
 
 }
