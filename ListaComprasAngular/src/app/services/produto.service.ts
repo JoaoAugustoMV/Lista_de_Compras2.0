04f73b757940b6025d3492a90b7108114420e382
JoaoA <joaootsugua@gmail.com>
2022-09-11 19:36:05 -0300

    Add metodos PUT e DELETE a pagina

--- ListaComprasAngular/src/app/services/produto.service.ts
+++ ListaComprasAngular/src/app/services/produto.service.ts
@@ -12,35 +12,42 @@ export class ProdutoService {
   private url = 'http://localhost:8080/api/listas/'
   private urlProdutos = 'http://localhost:8080/produtos/'
   constructor(private httpClient: HttpClient) {
+
   }
 
-  // GET
-  retornarTodos(){
-    this.httpClient.get(this.url).subscribe(resposta => console.log(resposta))
-  }
-  
-  // 
+
+  // GET: Retornar os nomes das lista
   retornarNomesListas(): Observable<string[]>{
     return this.httpClient.get<string[]>(this.url)
   }
 
-  // Get By nomeLista
-
+  // GET By nomeLista: Filtra a tabela pela coluna nome_lista
   retornarLista(nomeLista: string): Observable<Produto[]>{
     return this.httpClient.get<Produto[]>(this.url + nomeLista);
   }
-  //POST
+
+  //POST: Adicionar nova Lista
   adicionarLista(nomeLista: string, produtos: Produto[]): Observable<Produto[]>{ //  
     const url = this.url + nomeLista
     console.log("url", url)
     console.log("nomeLista", nomeLista)
     console.log("produtos", produtos)
 
-    
     return this.httpClient.post<Produto[]>(url, produtos)
   }
-    // post Lista: nomeLista, quant, totalLista
-    // post Produtos: nome, preco, quant, total
+
+  // PUT: Atualizar lista
+  atualizarLista(nomeLista: string,  produtos: Produto[]): Observable<Produto[]>{
+	console.log(this.url + nomeLista)
+    return this.httpClient.put<Produto[]>((this.url + nomeLista), produtos)
+  }
+  
+  // DELETE: Apagar todas ocorrencias de nomeLista
+  deletarLista(nomeLista: string): Observable<Produto[]>{
+    return this.httpClient.delete<Produto[]>(this.url + nomeLista)
+  }
+
+    
 
 
 }
