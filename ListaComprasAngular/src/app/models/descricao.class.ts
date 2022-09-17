8af0f64d67862e4b7b522aa33fced7ba77803893
JoaoA <joaootsugua@gmail.com>
2022-09-17 18:33:29 -0300

    Modelos de classes

---
+++ ListaComprasAngular/src/app/models/descricao.class.ts
@@ -0,0 +1,9 @@
+export class Descricao{
+    constructor(
+    public nomeLista: string,
+    public totalFinal: number
+    ){ 
+        this.nomeLista = nomeLista
+        this.totalFinal = totalFinal
+    }
+}
