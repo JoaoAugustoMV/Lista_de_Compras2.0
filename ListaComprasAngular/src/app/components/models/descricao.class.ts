12c54393847a6dd987763f6ad8535c218b505403
JoaoA <joaootsugua@gmail.com>
2022-09-17 18:42:26 -0300

    Deletando models da pasta errada

--- ListaComprasAngular/src/app/components/models/descricao.class.ts
+++
@@ -1,9 +0,0 @@
-export class Descricao{
-    constructor(
-    public nomeLista: string,
-    public totalFinal: number
-    ){ 
-        this.nomeLista = nomeLista
-        this.totalFinal = totalFinal
-    }
-}
